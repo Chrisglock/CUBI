c999cda8d687055c2e517fc8bb1a92bba44a64a0
christian esteban ramos baeza <cristianramos064@gmail.com>
2023-11-27 03:42:22 -0300

    5

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -138,8 +138,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'tasks'),)
 CKEDITOR_CONFIGS = {
    'default': {
        'toolbar_Full': [
